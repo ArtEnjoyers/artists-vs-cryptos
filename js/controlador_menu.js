37848c1b6c1ede80ca8a139b400ff1c48fc9a1d3
jordi110398 <jobe404@vidalibarraquer.net>
2022-05-13 16:22:27 +0200

    menu
    menu (index)

---
+++ js/controlador_menu.js
@@ -0,0 +1,15 @@
+function inicia_joc(){
+	loadpage("./html/joc.html");
+}
+
+function carrega_joc (){
+	loadpage("./html/carrega.html");
+}
+
+function surt (){
+
+}
+
+function opcions(){
+	loadpage("./html/opcions.html");
+}
